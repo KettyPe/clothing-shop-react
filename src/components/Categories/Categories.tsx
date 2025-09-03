3b32109ebfececf924fe5db70c049452a0c99a65
Yuliya <narutoworkzumaki@gmail.com>
2025-09-03 11:07:03 +0700

    feat: api catogories

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -1,56 +1,60 @@
+import React, { useEffect, useState } from "react";
 import "./style.css"
+
+
+interface Category {
+     Category_ID: number;
+     Category_Image: string;
+     Category_Name: string;
+     sort_order: number;
+}
 
 interface CardCategoriesProps {
      image: string
      title: string
+     name: string
 }
 
-const categoriesInfo = [
-     {
-          id: 1,
-          image: 'accessories',
-          title: "Аксессуары"
-     },
-     {
-          id: 2,
-          image: 't-shirts',
-          title: "Футболки"
-     },
-     {
-          id: 3,
-          image: 'hoodies',
-          title: "Толстовки"
-     },
-     {
-          id: 4,
-          image: 'jackets',
-          title: "Куртки"
-     },
-     {
-          id: 5,
-          image: 'pants',
-          title: "Брюки"
-     },
-     {
-          id: 6,
-          image: 'certificates',
-          title: "Сертификаты"
-     }
-]
-
 export const Categories = () => {
+     const [categories, setCategories] = useState<Category[]>([])
+     const [loading, setLoading] = useState(true);
+     const [error, setError] = useState<string | null>(null);
+
+     useEffect(() => {
+          const fetchCategories = async () => {
+               try {
+                    const response = await fetch('https://noxer-test.ru/webapp/api/products?on_main=true')
+                    if (!response.ok) throw new Error("Ошибка при загрузке категорий")
+                    const data = await response.json()
+                    setCategories(data.categories)
+               } catch (err) {
+                    setError((err as Error).message)
+               } finally {
+                    setLoading(false)
+               }
+          }
+
+          fetchCategories()
+     }, [])
+
+     if (loading) return <p>Загрузка категорий...</p>
+     if (error) return <p>Ошибка: {error}</p>
+
      return (
           <div className="categories">
                <div className="categories__container container">
                     <div className="categories__cards">
-                         {categoriesInfo.map((item) => {
-                              const { id, image, title } = item
+                         {categories
+                         .sort((a, b) => a.sort_order - b.sort_order)
+                         .map((item) => {
+                              const { Category_ID, Category_Image, Category_Name } = item
 
                               return (
                                    <CardCategories
-                                        key={id}
-                                        image={image}
-                                        title={title}
+                                        key={Category_ID}
+                                        image={Category_Image}
+                                        title={Category_Name}
+                                        name={Category_Name}
                                    />
                               )
                          })}
@@ -60,11 +64,14 @@ export const Categories = () => {
      )
 }
 
-const CardCategories = ({ image, title }: CardCategoriesProps) => {
+const CardCategories = ({ image, title, name}: CardCategoriesProps) => {
      return (
           <div className="categories__card">
                <a href='#' className="categories__image">
-                    <img src={`images/categories/${image}.jpg`} alt={`${image} image`} />
+                    <img
+                         src={image || `images/categories/pants.jpg`}
+                         alt={`${name} image`}
+                    />
                </a>
                <p className="categories__title">{title}</p>
           </div>
--- src/components/data.tsx
+++ src/components/data.tsx
@@ -26,3 +26,37 @@ export const footerMenuInfo = [
           icon: <AccountIcon />
      },
 ]
+
+
+export const categoriesInfo = [
+     {
+          id: 1,
+          image: 'accessories',
+          title: "Аксессуары"
+     },
+     {
+          id: 2,
+          image: 't-shirts',
+          title: "Футболки"
+     },
+     {
+          id: 3,
+          image: 'hoodies',
+          title: "Толстовки"
+     },
+     {
+          id: 4,
+          image: 'jackets',
+          title: "Куртки"
+     },
+     {
+          id: 5,
+          image: 'pants',
+          title: "Брюки"
+     },
+     {
+          id: 6,
+          image: 'certificates',
+          title: "Сертификаты"
+     }
+]
